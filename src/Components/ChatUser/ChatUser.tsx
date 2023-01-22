1db135d151c3ed053a661d44e40e0ee0e65a6d0e
Daniel Carmona <daniel.carmona@masglobalconsulting.com>
2023-01-22 14:32:13 -0500

    Fixing google img src 403 forbidden

--- src/Components/ChatUser/ChatUser.tsx
+++ src/Components/ChatUser/ChatUser.tsx
@@ -8,12 +8,18 @@ type ChatUserProps = {
 
 
 export const ChatUser : FC<ChatUserProps> = ({user}) => {
+
+    const startChat = () => {
+        console.log(user);
+    };
+
     return (
         <li className="p-2 border-bottom">
-            <a href="#!" className="d-flex justify-content-between text-decoration-none">
+            <button onClick={startChat} className="d-flex justify-content-between text-decoration-none">
                 <div className="d-flex flex-row">
                     <div>
-                        <img
+                        <img 
+                            referrerPolicy='no-referrer'
                             src={user.avatarUrl}
                             alt="avatar" className="d-flex align-self-center me-3" width="60" />
                         <span className="badge bg-success badge-dot"></span>
@@ -27,7 +33,7 @@ export const ChatUser : FC<ChatUserProps> = ({user}) => {
                     <p className="small text-muted mb-1">Just now</p>
                     <span className="badge bg-danger rounded-pill float-end">3</span>
                 </div>
-            </a>
+            </button>
         </li>
     );
 };
